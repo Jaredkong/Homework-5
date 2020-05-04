26de88fbc3ee62f03c9f0ac2044c80c6104a7ece
Jared Kong <jlkong001@yahoo.com>
2020-05-04 03:09:39 -0700

    added comments, updated README

--- README.md
+++ README.md
@@ -8,17 +8,19 @@ I WANT to add important events to a daily planner
 
 SO THAT I can manage my time effectively
 
-## Business Context
+## Description 
 
-Poor time management can result in missed meetings and deadlines or create the appearance of unprofessionalism. A daily planner allows employees to see their day at a glance, schedule time effectively, and improve productivity.
+The purpose of this project was to demonstrate the ability to use JQuery to dynamically create HTML.  As well as to save and retrieve from local storage.  It also showcases ability to utilize bootstrap, and to use a third party API.  
 
+![Alt text](./screenshot.jpg?raw=true "screenshot")
 
+The application is a calendar that allows a user to input an event for any hour between 9AM to 5pm.  The user is able to save this event, which pushes the input into local storage.  This allows the data to be saved even when the browser is refreshed.  In addition, the calendar is color-coded so that a user may easily see which tasks or events have passed, are in the present, or in the near future.  
 
-```
-GIVEN that an employee adds events to a specific hour in a calendar
+## Built with 
+Bootstrap
+JQuery
+moment.js
 
-WHEN the employee clicks the save button
+## Future Development
 
-THEN events are saved in the timeblock for that hour
-```
-
+The calendar could be coded to include other days besides the current one.  Another lacking feature is a clear local storage function, so that a user can reset their calendar.
--- script.js
+++ script.js
@@ -18,12 +18,12 @@ $(document).ready(function() {
             </div>
         </div>`
         );
-        // 
+        //Appends divs to starting container
         $(`#firstContainer`).append(newDivs);
         
 
     };
-
+    // Changes inner html of divs to reflect time
     $(`#time0`).html(`9am`);   
     $(`#time1`).html(`10am`);
     $(`#time2`).html(`11am`);
@@ -34,24 +34,26 @@ $(document).ready(function() {
     $(`#time7`).html(`4pm`);
     $(`#time8`).html(`5pm`);
 
-    
+    // Created an array to represent time in 24hr clock 
     let twentyFourHour = [9,10,11,12,13,14,15,16,17];
+    // Saves current hour
     let currentHour = moment().format('H');
 
-
+    // Checks if current time is in the 9am-5pm range
     if (twentyFourHour.includes(currentHour)) {
         let hourIndex = twentyFourHour.indexOf(currentHour);
 
-
+        //Changes current time to present
         $(`#input${hourIndex}`).addClass("present");
-
+        // Turns all hours up to current into past
         for (let i=0; i<hourIndex; i++) {
             $(`#input${i}`).addClass("past");
         };
-       
+       //Turns all hours past current to future
         for (let i=hourIndex +1; i<twentyFourHour.length; i++) {
             $(`#input${i}`).addClass("future");
         };
+    // If hour is before 9am change all to future
     } else if(currentHour <9){
 
         for (let i=0; i<twentyFourHour.length; i++) {
@@ -59,21 +61,22 @@ $(document).ready(function() {
         };
 
     } else {
-
+    //If not, change all to past
         for (let i=0; i<twentyFourHour.length; i++) {
             $(`#input${i}`).addClass("past");
         };
     };
-
+    // click event listener on saveBtn class
     $(".saveBtn").click(function saver() {
-        
+        // Grabbing index of this save button to identify which row to save
         let saveIndex = this.id.substring(4)
         let currentRow = $(`#input${saveIndex}`)
+        // Saves value of row in local storage with index as key
         localStorage.setItem(saveIndex, currentRow.val());
     });
-    
+    // For loop to iterate all rows
     for (let i=0; i<9; i++){
-
+        // Get local storage and put back into input value
         $(`#input${i}`).val(localStorage.getItem(i))
 
     };
