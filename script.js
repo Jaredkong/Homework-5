6b07987c94dfdb473802ed2e078fcc003cd002dd
Jared Kong <jlkong001@yahoo.com>
2020-05-03 23:12:56 -0700

    Dynamically created html

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(function() {
+    var firstContainer = $('#firstcontainer')
+    for (i=0; i<9; i++){
+        firstContainer.html(`<p> hey fag </p>`)
+        <div class = "">
+
+
+
+
+    };
+
+
+
+
+});
